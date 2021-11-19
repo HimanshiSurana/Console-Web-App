a214cbb4d63abf7dea649e599e8cef68013a0ca0
HimanshiSurana <94663112+HimanshiSurana@users.noreply.github.com>
2021-11-19 20:49:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My first console project!");
+var first_name="Ranbir";
+var last_name="Kapoor";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var i=3;
+var h=15;
+var add=h+i;
+console.log(add);
+var sub=h-i;
+console.log(sub);
+var mul=h*i;
+console.log(mul);
+var div=h/i;
+console.log(div)
